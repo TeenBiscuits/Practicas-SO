ae171b7570867c17b7429523e67109a918724c6e
Pablo Portas López <81629707+TeenBiscuits@users.noreply.github.com>
2024-09-21 19:38:48 +0200

    Devolver Path
    He sustituido el printf del directorio por un if else.
    En esencia se crea una variable cwd (current working directory) de tamaño path_max que es el tamaño máximo de una ruta en linux (lo saco de linux/limits.h) si se devuelve null es que se consiguió correctamente si no se imprime igualmente pero con error.

--- P0/p0.c
+++ P0/p0.c
@@ -2,14 +2,20 @@
 #include "stdlib.h"
 #include "string.h"
 #include "unistd.h"
+#include <linux/limits.h>
 
 #define MAX 2048
 #define MAX_ARG 1000
 
 int main(){
     char comando[MAX];
+    char cwd[PATH_MAX];
     while (1){
-        printf("/home/user/pablopablo's/shell> ");
+        if (getcwd(cwd,sizeof(cwd)) != NULL){
+            printf("pablo@ordenador-de-pablo:%s$ ",cwd);
+        }else{
+            printf("pablo@ordenador-de-pablo$ (getcwd return error) ");
+        }
         fgets(comando,MAX,stdin);
         comando[strcspn(comando, "\n")]=='\0';
         if (strcmp(comando, "exit")== '\0'){
