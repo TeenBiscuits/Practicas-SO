34e41b85a4307dda05c73aa2f82b66dfd41afdf1
paablomiiguez <pablommui@gmail.com>
2024-11-21 19:15:14 +0100

    memfill?

--- P2/memoria.c
+++ P2/memoria.c
@@ -1,12 +1,38 @@
-//
-// Created by Pablo Portas y Pablo Miguez
-//
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include "memoria.h"
 
-// Struct memory help para le help de los comandos estos
-struct MEMORYHELP CH[] = {
+// Función para convertir una dirección de memoria en formato string a void*
+void *convertir_direccion(const char *direccion_str) {
+    void *direccion = (void *)strtoull(direccion_str, NULL, 16);
+    if (direccion == NULL) {
+        printf("Error: dirección de memoria inválida.\n");
+    }
+    return direccion;
+}
 
-};
+// Función principal del comando memfill
+void Cmd_memfill(int NumTrozos, char *trozos[]) {
+    if (NumTrozos < 4) {
+        printf("Número de trozos incorrecto. Uso: memfill <addr> <ch> <cont>\n");
+        return;
+    }
+
+    void *direccion = convertir_direccion(trozos[1]);
+    if (direccion == NULL) return;
+
+    unsigned char caracter = (unsigned char)trozos[2][0];
+
+    size_t cantidad = strtoull(trozos[3], NULL, 10);
+    if (cantidad == 0) {
+        printf("Error: cantidad de bytes a llenar inválida.\n");
+        return;
+    }
+
+    //Aquí rellenamos memoria
+    memset(direccion, caracter, cantidad);
+
+    printf("llenando %zu bytes de memoria con el byte %c(%02X) a partir de la dirección %p\n",
+           cantidad, caracter, caracter, direccion);
+}
--- P2/memoria.h
+++ P2/memoria.h
@@ -5,4 +5,12 @@
 #ifndef MEMORIA_H
 #define MEMORIA_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+
+void Cmd_memfill(int NumTrozos, char *trozos[]);
 #endif //MEMORIA_H
