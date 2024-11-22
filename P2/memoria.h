9d29724ec781a8a8ce998b5f051b7c710c76b323
paablomiiguez <pablommui@gmail.com>
2024-11-22 18:47:10 +0100

    cabeceras de las funciones de memoria.c

--- P2/memoria.h
+++ P2/memoria.h
@@ -9,8 +9,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-
-
 void Cmd_memfill(int NumTrozos, char *trozos[]);
+
+void Cmd_memdump(int NumTrozos, char *trozos[]);
+
+void Cmd_memory(int numTrozos, char *args[]);
+
+void Cmd_readfile(int NumTrozos, char *args[]);
+
+void Cmd_writefile(int NumTrozos, char *args[]);
+
+void Cmd_read(int NumTrozos, char *args[]);
+
+void Cmd_write(int NumTrozos, char *args[]);
+
+
 #endif //MEMORIA_H
