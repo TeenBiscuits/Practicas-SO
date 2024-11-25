e9e57d4f2e65df5c28f0d4e84d1f653b71a521fb
Pablo Portas López <81629707+TeenBiscuits@users.noreply.github.com>
2024-11-25 11:53:34 +0100

    Fix malloc error <= 0 bytes

--- P3/memoria.c
+++ P3/memoria.c
@@ -19,21 +19,20 @@ void Cmd_allocate(int NumTrozos, char *trozos[]) {
 
     if (NumTrozos == 0) {
         MList_print(-1);
-    }
-    else {
-        void * paux;
+    } else {
+        void *paux;
         if (strcmp(trozos[1], "-malloc") == 0) {
             if (NumTrozos >= 2) {
-                paux = MList_add_malloc(atoi(trozos[2]));
-                if (paux == NULL) Aux_general_Imprimir_Error();
-                else printf(ANSI_COLOR_GREEN "Asignados %d bytes en %p" ANSI_COLOR_RESET "\n",atoi(trozos[2]),paux);
-            }
-            else MList_print(MALLOC);
+                if (atoi(trozos[2]) == 0)
+                    printf(ANSI_COLOR_RED "No se asignan bloques de 0 bytes." ANSI_COLOR_RESET "\n");
+                else if ((paux = MList_add_malloc(atoi(trozos[2]))) == NULL) Aux_general_Imprimir_Error();
+                else printf(ANSI_COLOR_GREEN "Asignados %d bytes en %p" ANSI_COLOR_RESET "\n", atoi(trozos[2]), paux);
+            } else MList_print(MALLOC);
         }
     }
 }
 
-void Cmd_deallocate(int NumTrozos, char *trozos[]){
+void Cmd_deallocate(int NumTrozos, char *trozos[]) {
     if (NumTrozos >= 1 && strcmp(trozos[1], "-?") == 0) {
         Help_deallocate();
         return;
@@ -41,8 +40,7 @@ void Cmd_deallocate(int NumTrozos, char *trozos[]){
 
     if (NumTrozos == 0) {
         MList_print(-1);
-    }
-    else {
+    } else {
         if (strcmp(trozos[1], "-malloc") == 0) {
             if (NumTrozos >= 2) MList_remove_malloc(atoi(trozos[2]));
             else MList_print(MALLOC);
@@ -50,37 +48,30 @@ void Cmd_deallocate(int NumTrozos, char *trozos[]){
     }
 }
 
-void Cmd_memfill(int NumTrozos, char *trozos[]){
-    
+void Cmd_memfill(int NumTrozos, char *trozos[]) {
 }
 
-void Cmd_memdump(int NumTrozos, char *trozos[]){
-    
+void Cmd_memdump(int NumTrozos, char *trozos[]) {
 }
 
-void Cmd_memory(int NumTrozos, char *trozos[]){
+void Cmd_memory(int NumTrozos, char *trozos[]) {
     if (NumTrozos >= 1 && strcmp(trozos[1], "-?") == 0) {
         Help_memory();
         return;
     }
 }
 
-void Cmd_readfile(int NumTrozos, char *trozos[]){
-    
+void Cmd_readfile(int NumTrozos, char *trozos[]) {
 }
 
-void Cmd_writefile(int NumTrozos, char *trozos[]){
-    
+void Cmd_writefile(int NumTrozos, char *trozos[]) {
 }
 
-void Cmd_read(int NumTrozos, char *trozos[]){
-    
+void Cmd_read(int NumTrozos, char *trozos[]) {
 }
 
-void Cmd_write(int NumTrozos, char *trozos[]){
-    
+void Cmd_write(int NumTrozos, char *trozos[]) {
 }
 
-void Cmd_recurse(int NumTrozos, char *trozos[]){
-    
+void Cmd_recurse(int NumTrozos, char *trozos[]) {
 }
