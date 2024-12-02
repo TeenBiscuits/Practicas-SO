0528660b7cea8a7ece97ce688dcbed3be5fa8ced
Pablo Portas López <81629707+TeenBiscuits@users.noreply.github.com>
2024-12-02 11:42:39 +0100

    Implementada ProcList Borrador

--- P3/proclist.c
+++ P3/proclist.c
@@ -1,4 +1,120 @@
 // Pablo Portas López           pablo.portas
 // Pablo Míguez Mouiño          pablo.miguez.moino
 
+#include <stdlib.h>
+
 #include "proclist.h"
+
+#include <string.h>
+#include <time.h>
+
+// Variable global del procedural
+tProcList procedural = {-1,NULL};
+
+
+// AUXILIARES / INTERNAS
+
+void PList_aux_createEmptyList(tProcList *lista) {
+    lista->contador = -1;
+    lista->start = PNULL;
+}
+
+bool PList_aux_isEmptyList(tProcList lista) {
+    if (lista.contador == -1) return true;
+    return false;
+}
+
+tPosProcL PList_aux_first(tProcList lista) {
+    return lista.start;
+}
+
+tPosProcL PList_aux_last(tProcList lista) {
+    tPosProcL puntero;
+    for (puntero = lista.start; puntero->siguiente != PNULL; puntero = puntero->siguiente);
+    return puntero;
+}
+
+tPosProcL PList_aux_next(tPosProcL posicion, tProcList lista) {
+    return posicion->siguiente;
+}
+
+tPosProcL PList_aux_previous(tPosProcL posicion, tProcList lista) {
+    if (posicion == lista.start) return PNULL;
+    tPosProcL aux;
+    for (aux = lista.start; aux->siguiente != posicion; aux = aux->siguiente);
+    return aux;
+}
+
+bool PList_aux_insertItem(pid_t pid, time_t time, tCommandLine command, tPosProcL posicion, tProcList *lista) {
+    if (!PList_aux_isEmptyList(*lista) && lista->contador == MAX_PLIST_SIZE) return false;
+
+    tPosProcL aux1 = malloc(sizeof(struct tNodeProc));
+
+    if (aux1 == NULL) return false;
+
+    // Creación del nodo
+    aux1->pid = pid;
+    aux1->time = time;
+    strcpy(aux1->command, command);
+    aux1->siguiente = PNULL;
+
+    if (lista->start == PNULL)
+        lista->start = aux1;
+    else if (posicion == PNULL) {
+        tPosProcL aux2 = PList_aux_last(*lista);
+        aux2->siguiente = aux1;
+    } else if (posicion == PList_aux_first(*lista)) {
+        aux1->siguiente = posicion;
+        lista->start = aux1;
+    } else {
+        tPosProcL aux2 = PList_aux_previous(posicion, *lista);
+        aux2->siguiente = aux1;
+        aux1->siguiente = posicion;
+    }
+
+    lista->contador += 1;
+    return true;
+}
+
+void PList_aux_deleteAtPosition(tPosProcL posicion, tProcList *lista) {
+    tPosProcL aux, anterior;
+
+    if (PList_aux_next(posicion, *lista) == PNULL) {
+        if (posicion == lista->start) {
+            lista->start = PNULL;
+        } else {
+            aux = PList_aux_previous(posicion, *lista);
+            aux->siguiente = PNULL;
+        }
+    } else {
+        if (posicion == lista->start) {
+            lista->start = PList_aux_next(posicion, *lista);
+        } else {
+            anterior = PList_aux_previous(posicion, *lista);
+            anterior->siguiente = PList_aux_next(posicion, *lista);
+        }
+    }
+    lista->contador -= 1;
+    free(posicion);
+}
+
+/*
+void PList_aux_printNode(tPosProcL posicion) {
+    char datebuffer[80];
+    struct tm *tm_info = localtime(&posicion->time);
+    strftime(datebuffer, sizeof(datebuffer), "%d %b %y %H:%M", tm_info);
+    switch (posicion->alloc) {
+        case MALLOC:
+            printf("\t%p %15d %s %s\n", posicion->address, posicion->size, datebuffer, "malloc");
+            break;
+        case SHARED:
+            printf("\t%p %15d %s %s (key %d)\n", posicion->address, posicion->size, datebuffer, "shared",
+                   posicion->smb_key);
+            break;
+        case MAPPED:
+            printf("\t%p %15d %s %s (descriptor %d)\n", posicion->address, posicion->size, datebuffer,
+                   posicion->file_name, posicion->file_desc);
+            break;
+    }
+}
+*/
--- P3/proclist.h
+++ P3/proclist.h
@@ -4,4 +4,62 @@
 #ifndef PROLIST_H
 #define PROLIST_H
 
+#include <stdbool.h>
+#include <sys/types.h>
+
+#define PNULL NULL              // Elemento nulo de la lista, equivalente pero no reemplazable por NULL
+#define MAX_PLIST_SIZE 1000     // Tamaño máximo de la lista
+
+// Tipo de dato posición de lista, es un puntero a un nodo
+typedef struct tNodeProc *tPosProcL;
+
+typedef void *tAddressL;
+
+enum tStatusL { FINISHED, STOPPED, SIGNALED, ACTIVE };
+
+typedef char tCommandLine[2048];
+
+// Struct de nodo, formado por la información guardada y un puntero al siguiente elemento.
+struct tNodeProc {
+    pid_t pid; // PID
+    time_t time; // Fecha de lanzamiento
+    enum tStatusL status; // Estado
+    tCommandLine command; // Command Line
+    tPosProcL siguiente; // Puntero al siguiente nodo de la lista
+};
+
+// Tipo dato lista, compuesto de un contador de elementos y un puntero al primer elemento
+typedef struct tProcList {
+    int contador;
+    tPosProcL start;
+} tProcList;
+
+
+// AUXILIARES / INTERNAS
+
+// Dado la dirección de memoria de una variable tProcList, inicializar una lista vacía
+void PList_aux_createEmptyList(tProcList *lista);
+
+// Comprueba si una lista (tProcList) está vacía
+bool PList_aux_isEmptyList(tProcList lista);
+
+// Devuelve la posición del primer elemento de la lista
+tPosProcL PList_aux_first(tProcList lista);
+
+// Devuelve la posición del último elemento de la lista
+tPosProcL PList_aux_last(tProcList lista);
+
+// Devuelve la posición del elemento siguiente a la posición dada
+tPosProcL PList_aux_next(tPosProcL posicion, tProcList lista);
+
+// Devuelve la posición del elemento anterior a la posición dada
+tPosProcL PList_aux_previous(tPosProcL posicion, tProcList lista);
+
+// Inserta un item en la posición indicada, de no indicarse ninguna (MNULL) se añade al final de la lista.
+// Devuelve true/false dependiendo de si se ha podido o no insertar a la lista. El tamaño máximo es 1000.
+bool PList_aux_insertItem(pid_t pid, time_t time, tCommandLine command, tPosProcL posicion, tProcList *lista);
+
+// Elimina el elemento de la posición indicada. No se devuelve si la eliminación fue ejecutada.
+void PList_aux_deleteAtPosition(tPosProcL posicion, tProcList *lista);
+
 #endif //PROLIST_H
